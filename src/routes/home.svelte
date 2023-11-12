fabeaaa2c5447ddcd5a40e5936ea797cc35ffb97
zl90 <101400977+zl90@users.noreply.github.com>
2023-11-12 20:45:15 +1100

    Copy change

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -11,11 +11,18 @@
     <Avatar />
 
     <p>
-      I'm Matt. I build stuff with React, Typescript and AWS. I also
-      occasionally work on artistic endeavours. These are my thoughts, opinions
-      and lessons learned. My goal is to help junior developers avoid the
-      mistakes I've made in the past. Find me on LinkedIn if you are so
-      inclined. Opinions are my own and do not represent those of my employer.
+      I'm Matt. I build stuff with React, Typescript and AWS. I have an
+      insatiable appetite for learning and in my spare time I'm always improving
+      my skills (I'm currently taking a course on Compiler design from Cornell
+      University: <a href="https://github.com/zl90/Cornell-CS4120"
+        >link to the repo</a
+      >). These are my thoughts, opinions and lessons learned. My goal is to
+      inspire people to improve their tech skills. Lately I've discovered the
+      power of working towards your goal with 1-2 hours per day of deliberate
+      practice. With that time investment, you'd be shocked at how much you can
+      learn in only 1 year. Find me on
+      <a href="https://www.linkedin.com/in/matthew-robert-111182248">LinkedIn</a
+      > if you want.
     </p>
 
     <BlogList />
