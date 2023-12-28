b9720bfc661e3de6a8b49cc8331055c92e3b05f2
zl90 <101400977+zl90@users.noreply.github.com>
2023-12-28 14:34:01 +1100

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,8 @@
 <header>
-  <div class="title"><a href="/">mb541~</a></div>
+  <div class="title"><a href="/">~/mb541</a></div>
   <div class="menu">
     <ul>
-      <li><a href="/">/</a></li>
+      <li><a href="~/">/</a></li>
       <li><a href="/portfolio">portfolio</a></li>
       <li><a href="/cv">cv</a></li>
     </ul>
